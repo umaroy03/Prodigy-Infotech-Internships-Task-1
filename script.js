de66068cd37a95776894d61b7ebb17917399ee2c
umaroy03 <132841120+umaroy03@users.noreply.github.com>
2024-05-18 14:07:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#00bfff';
+    } else {
+        navbar.style.backgroundColor = '#00bfff';
+    }
+});
